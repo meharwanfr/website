1beeaf8f4e8e78e855a7143aee6257ba71fe7092
meharwanfr <meharwansinghji@gmail.com>
2026-08-10 13:12:42 +0530

    feat: worked a wip feature to play bad apple on a 2d grid (still wip) and finally implemented a simple blogging system for my site.

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection } from "astro/content/config";
+
+import { z } from "astro/zod";
+
+import { glob, file } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ base: "./src/pages/blogs", pattern: "**/*.{md,mdx}" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    draft: z.boolean().default(false),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
+
+export const collections = { blog }
---
+++ src/pages/blog/template.md
@@ -0,0 +1,20 @@
+---
+
+layout: ../../layouts/BlogLayout.astro
+
+title: "Template Blog (heheh)"
+description: "This is just a template blog to test out my blog's ui and its just here for the demo, I may remove this in future tho."
+pubDate: 2026-08-10
+updatedDate: 2026-08-10
+tags:
+  - astro
+  - web-dev
+  - template
+author: "Meharwan Singh"
+image: "/src/assets/blog_imgs/template.jpg"
+draft: false
+---
+
+# How I Built My Website
+
+Astro is an excellent framework for content-focused websites...
